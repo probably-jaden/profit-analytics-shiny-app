3b27c6527064417144cc0250526f8d232926f3be
Jaden Earl <jadendwightearl@gmail.com>
2026-04-15 11:42:56 -0600

    improved test file

--- ProfitAnalytics/app.R
+++ ProfitAnalytics/app.R
@@ -4,3 +4,9 @@ source("ui.R")
 source("server.R")
 
 shinyApp(ui = ui, server = server)
+
+cupcakes <- read_csv("~/Documents/projects/Entrepreneurship Analytics/profit-analytics-shiny-app/CupcakesTest2.csv")
+
+cupcakes2 <- cupcakes %>% dplyr::select(c(cupcakes, donuts))
+
+write.csv(cupcakes2, file = "cupcakes.csv")
